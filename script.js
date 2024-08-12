96b805c059de366c0a1a7a30ea6955a3e3a4f62b
yasmim-vitor <168394841+yasmim-vitor@users.noreply.github.com>
2024-08-12 09:31:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const avanca = document.querySelectorAll('.btn-proximo')
+const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
